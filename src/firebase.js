574ec900675c9fc959691d885c45f97e97559d91
ugochukwuomeje <ugochukwuomeje1@gmail.com>
2022-02-27 22:38:34 +0100

    create header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD02RxIW9iukBKntMh5tjuAgOl9iCXY7Ro",
+    authDomain: "challenge-c573f.firebaseapp.com",
+    projectId: "challenge-c573f",
+    storageBucket: "challenge-c573f.appspot.com",
+    messagingSenderId: "1039543763111",
+    appId: "1:1039543763111:web:a276e9d2bf46fa65c55b82"
+  };
